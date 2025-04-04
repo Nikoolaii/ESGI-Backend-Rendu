5e620e102efc085775a63b2fe3cac099801ef4eb
Nikoolaii <nikolailemerre@gmail.com>
2025-04-04 16:27:45 +0200

    ♻️ Refactor comment routes and schema for clarity

--- controller/Commentaire.controller.js
+++ controller/Commentaire.controller.js
@@ -3,14 +3,15 @@ const User = require("./../model/user.model.js");
 
 
 exports.addComment = async (req, res) => {
-    try {
-        const { text } = req.body;
-const userId = req.User.id;
+  try {
+    const { text } = req.body;
+    const userId = "1";
     const { postId } = req.params;
-         const newComment = new Comment({ userId, postId, text });
-            await newComment.save();
-        res.status(201).json({ message: "Commentaire ajouté avec succès", comment: newComment });
-    } catch (error) {
-        res.status(500).json({ error: "Erreur lors de l'ajout du commentaire" });
-    }
+    const newComment = new Comment({ userId, postId, text });
+    await newComment.save();
+    res.status(201).json({ message: "Commentaire ajouté avec succès", comment: newComment });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: "Erreur lors de l'ajout du commentaire" });
+  }
 };
